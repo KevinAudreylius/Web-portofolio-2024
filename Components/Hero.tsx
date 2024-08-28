fdbd81c625646a67cb3d037468c51313b8c59920
KevinAudreylius <kevinlius4504@gmail.com>
2024-08-28 11:44:01 +0700

    added

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
                 Welcome to my creative portofolio. This website will help you to explore my work, discover my journey, and let's create something amazing together.
             </p>
             <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
-                <a href="https://github.com/KevinAudreylius/Portofolio-2024/blob/asset/CV%20.pdf" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/KevinAudreylius/Web-portofolio-2024/blob/1654c7d33ad59efa8de61957e19e820ff151f2a7/CV%20.pdf" target="_blank" rel="noopener noreferrer">
                     <button className='px-[1.8rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#9D59EF] text-black flex items-center space-x-2'>
                         <p> View CV </p>
                         <ArrowDownTrayIcon className='w-[2rem] h-[1.7rem] text-black'/>
