35065c345be23d5a61c615b16844e01b7939bf62
KevinAudreylius <kevinlius4504@gmail.com>
2026-01-05 16:39:28 +0700

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import About from '@/components/About';
+import About from '../components/About';
 import Background from '@/components/Background';
 import Blog from '@/components/Blog';
 import Experiences from '@/components/Experiences';
